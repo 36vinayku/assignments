2bee2ec655935bae4c2d581929c583f263b0aa7e
vinay kumar c <36vinayku@gmail.com>
2018-02-11 15:07:21 +0530

    first commit

---
+++ c1.py
@@ -0,0 +1,22 @@
+class Pointer(object):
+
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+
+	def moveup(self):
+		self.y+=1
+
+	def movedowm(self):
+		self.y-=1
+
+	def moveright(self):
+		self.x+=1
+
+	def moveleft(self):
+		self.x-=1
+
+	def checkpos(self):
+		x=self.x
+		y=self.y
+		print(x,y)
